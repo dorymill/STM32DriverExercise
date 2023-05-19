c35430191a7df509ec1ea192e016143ac2877a23
Apollo.Secondary <dorymill@outlook.com>
2023-05-19 00:11:48 -0400

    Finished GetBearing and SingleShotRead methods. Added further encapsulation.

--- DeviceDrivers/Src/HMC5883L.c
+++ DeviceDrivers/Src/HMC5883L.c
@@ -6,6 +6,9 @@
  */
 
 #include "HMC5883L.h"
+#include <math.h>
+
+#define PI 3.1415926
 
  
 /*
@@ -28,7 +31,7 @@ void InitCompass(I2C_Handle_t    *pI2CHandle,
 {
 
     uint8_t temp = 0;
-    uint8_t cmd[3] = {COMPASS_REG_WRITE,0,0};
+    uint8_t (*Txcmd) [3];
 
     pCompass->devAddr = COMPASS_ADDR;
 
@@ -37,27 +40,24 @@ void InitCompass(I2C_Handle_t    *pI2CHandle,
            (compassCfg.output_rate  << COMPASS_CRA_DO0)  |
            (compassCfg.bias_control << COMPASS_CRA_MS0);
 
-    *(cmd + 1) = COMPASS_CFGA;
-    *(cmd + 2) = temp;
+    Txcmd = commandGenTx(COMPASS_CFGA, temp);
 
-    I2C_MasterTx(pI2CHandle, cmd, 3, pCompass->devAddr);
+    I2C_MasterTx(pI2CHandle, &Txcmd, 3, pCompass->devAddr);
 
     /* Configure CRB */
     temp = (compassCfg.gain_control << COMPASS_CRB_GN);
 
-    *(cmd + 1) = COMPASS_CFGB;
-    *(cmd + 2) = temp;
+    Txcmd = commandGenTx(COMPASS_CFGB, temp);
 
-    I2C_MasterTx(pI2CHandle, cmd, 3, pCompass->devAddr);
+    I2C_MasterTx(pI2CHandle, &Txcmd, 3, pCompass->devAddr);
 
     /* Configure Mode */
     temp = (compassCfg.op_mode << COMPASS_MODE_MDO) |
            (compassCfg.hsbit   << COMPASS_MODEHS);
 
-    *(cmd + 1) = COMPASS_MODE;
-    *(cmd + 2) = temp;
+	Txcmd = commandGenTx(COMPASS_MODE, temp);
 
-    I2C_MasterTx(pI2CHandle, cmd, 3, pCompass->devAddr);
+    I2C_MasterTx(pI2CHandle, &Txcmd, 3, pCompass->devAddr);
 
 }
 
@@ -79,9 +79,24 @@ float GetBearing (I2C_Handle_t    *pI2CHandle,
                   CompassHandle_t *pCompass)
 {
 
-       SingleShotRead(pI2CHandle, pCompass);
+	SingleShotRead(pI2CHandle, pCompass);
 
-       return pCompass->compDat->bearing;
+	float temp = 0;
+
+	temp = (180 / PI) * atan2(pCompass->compDat->y_msmnt, 
+							  pCompass->compDat->x_msmnt);
+
+	while (temp > 360) {
+		temp -= 360;
+	}
+
+	while(temp < 0) {
+		temp += 360;
+	}
+
+	pCompass->compDat->bearing = temp;
+
+	return temp;
 }
 
 
@@ -95,4 +110,76 @@ float GetBearing (I2C_Handle_t    *pI2CHandle,
 void SingleShotRead(I2C_Handle_t *pI2CHandle, CompassHandle_t *pCompass)
 {
 
+	uint8_t (*data)[6];
+
+	uint8_t (*Rxcmd)[2];
+
+	volatile uint8_t dataflag = 0;
+
+	/* Check status register for data ready flag. */
+	while(dataflag) { 
+		Rxcmd = commandGenRx(COMPASS_SR_RDY);
+
+		I2C_MasterTx(pI2CHandle,  Rxcmd,    2, COMPASS_ADDR);
+		I2C_MasterRx(pI2CHandle, &dataflag, 1, COMPASS_ADDR);
+		}
+
+	/* Read in axis data values */
+	for (int iter = 0; iter < 6; iter++) {
+		Rxcmd = commandGenRx(COMPASS_DR+iter);
+		
+		I2C_MasterTx(pI2CHandle,  Rxcmd,         2, COMPASS_ADDR);
+		I2C_MasterRx(pI2CHandle, *(data + iter), 1, COMPASS_ADDR);
+
+	}
+	  
+	/* Store X data */
+	pCompass->compDat->x_msmnt = (int16_t) ((*data[0] & 0xFF00) << 8) |
+										   ((*data[1] & 0x00FF) << 0);
+
+	/* Store Y data */
+	pCompass->compDat->x_msmnt = (int16_t) ((*data[4] & 0xFF00) << 8) |
+										   ((*data[5] & 0x00FF) << 0);
+
+	/* Store Z data */
+	pCompass->compDat->x_msmnt = (int16_t) ((*data[2] & 0xFF00) << 8) |
+										   ((*data[2] & 0x00FF) << 0);
+}
+
+/**
+ * @brief Fill a 2 bute array consisting of a read byte,
+ *        and the compass register address to read from.
+ * 
+ * @param address   Compass register address
+ * @return uint8_t* Pointer to crafted command array
+ */
+uint8_t *commandGenRx (uint8_t address) {
+
+	uint8_t command [2];
+
+	*(command + 0) = COMPASS_REG_READ;
+	*(command + 1) = address;
+
+	return &command;
+}
+
+
+/**
+ * @brief Fill a 3 byte array consisting of a write byte, an
+ *        address byte, and a data byte. This can then be sent
+ *        using I2C_MasterTx.
+ * 
+ * @param address   Compass register address
+ * @param txDat     Data byte to write
+ * @return uint8_t* Pointer to crafted command array
+ */
+uint8_t *commandGenTx (uint8_t address, uint8_t txDat) {
+
+	uint8_t command [3];
+
+	*(command + 0) = COMPASS_REG_WRITE;
+	*(command + 1) = address;
+	*(command + 2) = txDat;
+
+	return &command;
 }
